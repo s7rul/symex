d280906ed052ff5d48846ff6de7303989a803b6b
s7rul <serraerik1@gmail.com>
2023-10-13 22:10:25 +0200

    wip

--- symex/src/general_assembly/instruction.rs
+++ symex/src/general_assembly/instruction.rs
@@ -7,10 +7,26 @@ pub struct Instruction {
 }
 
 pub enum Operation {
+    /// No operation
     Nop,
+
+    /// Moves the value in the source to the destination.
+    /// If source is an address it is loaded from memmory
+    /// and if destination is an address it is stored into memmory.
+    Move{destination: Operand, source: Operand},
+
+    /// Adds
+    /// destination = operand1 + operand2
+    Add{destination: Operand, operand1: Operand, operand2: Operand},
+
+    /// Subtracts
+    /// destination = operand1 - operand2
+    Sub{destination: Operand, operand1: Operand, operand2: Operand},
 }
 
 pub enum Operand {
     Register(String),
     Immidiate(DataWord),
+    Address(DataWord),
+    AddressWithOffset{address: DataWord, offset_reg: String},
 }
