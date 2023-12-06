c6051af9c774dba8b1f18819b005a4ebcb205a17
s7rul <serraerik1@gmail.com>
2023-12-06 15:30:31 +0100

    added arm-m0+ cycle counts

--- symex/src/general_assembly/translators/armv6_m.rs
+++ symex/src/general_assembly/translators/armv6_m.rs
@@ -24,6 +24,135 @@ type GAInstruction = crate::general_assembly::instruction::Instruction;
 type GAOperation = crate::general_assembly::instruction::Operation;
 type ArmCodition = armv6_m_instruction_parser::conditions::Condition;
 
+fn cycle_count_m0plus_core(operation: &Operation) -> CycleCount {
+    match operation {
+        Operation::ADCReg { m: _, n: _, d: _ } => CycleCount::Value(1),
+        Operation::ADDImm { imm: _, n: _, d: _ } => CycleCount::Value(1),
+        Operation::ADDReg { m: _, n: _, d } => {
+            let max_cycle = if *d == Register::PC { 2 } else { 1 };
+            CycleCount::Value(max_cycle)
+        }
+        Operation::ADDImmSP { d: _, imm: _ } => CycleCount::Value(1),
+        Operation::ADDRegSP { d: _, m: _ } => CycleCount::Value(1),
+        Operation::ADR { d: _, imm: _ } => CycleCount::Value(1),
+        Operation::ANDReg { m: _, dn: _ } => CycleCount::Value(1),
+        Operation::ASRImm { imm: _, m: _, d: _ } => CycleCount::Value(1),
+        Operation::ASRReg { m: _, dn: _ } => CycleCount::Value(1),
+        Operation::B { cond: _, imm: _ } => {
+            let max_cycle: fn(state: &GAState) -> usize = |state| {
+                if state.get_has_jumped() {
+                    2
+                } else {
+                    1
+                }
+            };
+            CycleCount::Function(max_cycle)
+        }
+        Operation::BICReg { m: _, dn: _ } => CycleCount::Value(1),
+        Operation::BKPT { imm: _ } => CycleCount::Value(0),
+        Operation::BL { imm: _ } => CycleCount::Value(3),
+        Operation::BLXReg { m: _ } => CycleCount::Value(2),
+        Operation::BX { m: _ } => CycleCount::Value(2),
+        Operation::CMNReg { m: _, n: _ } => CycleCount::Value(1),
+        Operation::CMPImm { n: _, imm: _ } => CycleCount::Value(1),
+        Operation::CMPReg { m: _, n: _ } => CycleCount::Value(1),
+        Operation::CPS { im: _ } => CycleCount::Value(1),
+        Operation::CPY => {
+            // this is not a real instruction is equvelatn to mov
+            unreachable!()
+        }
+        Operation::DMB { option: _ } => CycleCount::Value(3),
+        Operation::DSB { option: _ } => CycleCount::Value(3),
+        Operation::EORReg { m: _, dn: _ } => CycleCount::Value(1),
+        Operation::ISB { option: _ } => CycleCount::Value(3),
+        Operation::LDM { n: _, reg_list } => {
+            let max_cycle = 1 + reg_list.len();
+            CycleCount::Value(max_cycle)
+        }
+
+        // \/\/\/\/ Can be one depending on core implementation and address \/\/\/\/
+        Operation::LDRImm { imm: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::LDRLiteral { t: _, imm: _ } => CycleCount::Value(2),
+        Operation::LDRReg { m: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::LDRBImm { imm: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::LDRBReg { m: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::LDRHImm { imm: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::LDRHReg { m: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::LDRSBReg { m: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::LDRSH { m: _, n: _, t: _ } => CycleCount::Value(2),
+        // /\/\/\/\ Can be one depending on core implementation and address /\/\/\/\
+
+        Operation::LSLImm { imm: _, m: _, d: _ } => CycleCount::Value(1),
+        Operation::LSLReg { m: _, dn: _ } => CycleCount::Value(1),
+        Operation::LSRImm { imm: _, m: _, d: _ } => CycleCount::Value(1),
+        Operation::LSRReg { m: _, dn: _ } => CycleCount::Value(1),
+        Operation::MOVImm { d: _, imm: _ } => CycleCount::Value(1),
+        Operation::MOVReg {
+            m: _,
+            d,
+            set_flags: _,
+        } => {
+            let max_cycle = if *d == Register::PC { 2 } else { 1 };
+            CycleCount::Value(max_cycle)
+        }
+        Operation::MRS { d: _, sysm: _ } => CycleCount::Value(3),
+        Operation::MSRReg { n: _, sysm: _ } => CycleCount::Value(3),
+        Operation::MUL { n: _, dm: _ } => {
+            CycleCount::Value(32) // Can be one depending on core implementation might be able to read this from somewhere.
+        }
+        Operation::MVNReg { m: _, d: _ } => CycleCount::Value(1),
+        Operation::NOP => CycleCount::Value(1),
+        Operation::ORRReg { m: _, dn: _ } => CycleCount::Value(1),
+        Operation::POP { reg_list } => {
+            let max_cycle = if reg_list.contains(&Register::PC) {
+                3
+            } else {
+                1
+            } + reg_list.len();
+            CycleCount::Value(max_cycle)
+        }
+        Operation::PUSH { reg_list } => CycleCount::Value(1 + reg_list.len()),
+        Operation::REV { m: _, d: _ } => CycleCount::Value(1),
+        Operation::REV16 { m: _, d: _ } => CycleCount::Value(1),
+        Operation::REVSH { m: _, d: _ } => CycleCount::Value(1),
+        Operation::RORReg { m: _, dn: _ } => CycleCount::Value(1),
+        Operation::RSBImm { n: _, d: _ } => CycleCount::Value(1),
+        Operation::SBCReg { m: _, dn: _ } => CycleCount::Value(1),
+        Operation::SEV => CycleCount::Value(1),
+        Operation::STM { n: _, reg_list } => CycleCount::Value(1 + reg_list.len()),
+
+        // \/\/\/\/ Can be one depending on core implementation and address \/\/\/\/
+        Operation::STRImm { imm: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::STRReg { m: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::STRBImm { imm: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::STRBReg { m: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::STRHImm { imm: _, n: _, t: _ } => CycleCount::Value(2),
+        Operation::STRHReg { m: _, n: _, t: _ } => CycleCount::Value(2),
+        // /\/\/\/\ Can be one depending on core implementation and address /\/\/\/\
+
+        Operation::SUBImm { imm: _, n: _, d: _ } => CycleCount::Value(1),
+        Operation::SUBReg { m: _, n: _, d: _ } => CycleCount::Value(1),
+        Operation::SUBImmSP { imm: _ } => CycleCount::Value(1),
+        Operation::SVC { imm: _ } => CycleCount::Value(0),
+        Operation::SXTB { m: _, d: _ } => CycleCount::Value(1),
+        Operation::SXTH { m: _, d: _ } => CycleCount::Value(1),
+        Operation::TSTReg { m: _, n: _ } => CycleCount::Value(1),
+        Operation::UDFT1 { imm: _ } => {
+            // generates a undefined exeption just panic for now
+            unimplemented!()
+        }
+        Operation::UDFT2 { imm: _ } => {
+            // generates a undefined exeption just panic for now
+            unimplemented!()
+        }
+        Operation::UXTB { m: _, d: _ } => CycleCount::Value(1),
+        Operation::UXTH { m: _, d: _ } => CycleCount::Value(1),
+        Operation::WFE => todo!(),
+        Operation::WFI => todo!(),
+        Operation::YIELD => todo!(),
+    }
+}
+
 fn cycle_count_m0_core(operation: &Operation) -> CycleCount {
     match operation {
         Operation::ADCReg { m: _, n: _, d: _ } => CycleCount::Value(1),
@@ -1452,7 +1581,7 @@ impl Translatable for Instruction {
             armv6_m_instruction_parser::instructons::InstructionWidth::Bit16 => 16,
         };
 
-        let max_cycle_count = cycle_count_m0_core(&self.operation);
+        let max_cycle_count = cycle_count_m0plus_core(&self.operation);
 
         GAInstruction {
             instruction_size: instruction_width,
