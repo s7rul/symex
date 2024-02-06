c5e961f0ddf616118d772fa934522e9368b1197e
s7rul <serraerik1@gmail.com>
2024-02-06 11:00:16 +0100

    Removed not yet working examples and tmp files
    Removed examples from armv6-m examples that where not working and some
    dumped temporary files that had been pushed by mistake.

--- armv6-m-examples/examples/test_functions.rs
+++ armv6-m-examples/examples/test_functions.rs
@@ -145,18 +145,6 @@ fn test_any_u64() -> u64 {
     }
 }
 
-#[inline(never)]
-fn test_assume_llvm() -> u8 {
-    let n = any();
-    test_assume(n)
-}
-
-#[inline(never)]
-fn simple_loop_llvm() -> u8 {
-    let n = any();
-    simple_loop(n)
-}
-
 fn run_function(f: fn(u8) -> u8) {
     for n in 0..u8::MAX {
         let r = f(n);
